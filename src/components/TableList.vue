26f1efe6886e02f503bd26e5ac602419a6c6a7b9
Dereck Lam Hon Wah <dereck.lamhonwah@gmail.com>
2024-10-08 20:04:35 +0400

    fix: align top of table data in small screen

--- src/components/TableList.vue
+++ src/components/TableList.vue
@@ -4,10 +4,10 @@
   >
     <tbody>
       <tr v-for="(item, index) in items" :key="index">
-        <td class="text-left w-1/12">{{ item.date }}</td>
-        <td class="text-left font-bold w-3/12">{{ item.title }}</td>
-        <td class="text-left w-4/12">{{ item.event }}</td>
-        <td class="text-right italic w-4/12">
+        <td class="text-left w-1/12 align-top">{{ item.date }}</td>
+        <td class="text-left font-bold w-3/12 align-top">{{ item.title }}</td>
+        <td class="text-left w-4/12 align-top">{{ item.event }}</td>
+        <td class="text-right italic w-4/12 align-top">
           {{ item.location }}
         </td>
       </tr>
