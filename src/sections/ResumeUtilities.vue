f9f8d29332815d4a607f1d8f56acd600496d3c7a
Dereck Lam Hon Wah <dereck.lamhonwah@gmail.com>
2024-10-09 14:51:14 +0400

    refac: slot machine visitor counter

--- src/sections/ResumeUtilities.vue
+++ src/sections/ResumeUtilities.vue
@@ -1,9 +1,12 @@
 <template lang="">
   <div
-    :class="`flex flex-col-reverse md:flex-row justify-center text-center gap-6 font-bold text-lg print:hidden ${!resumeStore.getVisitorCount ? 'md:justify-end' : 'md:justify-between'}`"
+    :class="`flex flex-col-reverse md:flex-row justify-center md:justify-between text-center gap-6 font-bold text-lg print:hidden`"
   >
-    <p v-if="resumeStore.getVisitorCounterEnabled && resumeStore.getVisitorCount" class="underline">
-      Visitor Counter: {{ resumeStore.getVisitorCount }}
+    <p v-if="resumeStore.getVisitorCounterEnabled && resumeStore.getVisitorCount">
+      Visitor Counter: {{ displayedCount }}
+    </p>
+    <p v-else-if="resumeStore.getVisitorCounterEnabled && !resumeStore.getVisitorCount">
+      Visitor Counter: ...
     </p>
     <Button @click="print" class="hover:bg-gray-50 border-2 font-bold py-2 px-4 rounded"
       >Print my CV
@@ -11,10 +14,11 @@
   </div>
 </template>
 <script setup lang="ts">
-import { onMounted, onUnmounted } from 'vue'
+import { onMounted, onUnmounted, ref, watch } from 'vue'
 import { useResumeStore } from '@/stores/resume'
 
 const resumeStore = useResumeStore()
+const displayedCount = ref(0)
 
 onMounted(() => {
   const intervalId = setInterval(() => {
@@ -26,8 +30,36 @@ onMounted(() => {
   })
 })
 
+watch(
+  () => resumeStore.getVisitorCount,
+  (newCount) => {
+    if (newCount > 0) {
+      animateCounter()
+    }
+  }
+)
+
 const print = () => {
   window.print()
+}
+
+const animateCounter = () => {
+  const duration = 2000 // Animation duration in milliseconds
+  const start = displayedCount.value
+  const end = resumeStore.getVisitorCount
+  const startTime = performance.now()
+
+  function update(currentTime: number) {
+    const elapsedTime = currentTime - startTime
+    if (elapsedTime < duration) {
+      displayedCount.value = Math.round(start + (end - start) * (elapsedTime / duration))
+      requestAnimationFrame(update)
+    } else {
+      displayedCount.value = end
+    }
+  }
+
+  requestAnimationFrame(update)
 }
 </script>
 <style lang=""></style>
